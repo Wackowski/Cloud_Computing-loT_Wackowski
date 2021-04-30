30b67aac0bf40cccf83f8d7696d0a457357dc702
Bartosz Wackowski <bartosz.wackowski0@gmail.com>
2021-04-30 17:18:11 +0200

    Update Startup.cs
    wersja ostateczna

--- intro docker/DockerExample.WebApp/Startup.cs
+++ intro docker/DockerExample.WebApp/Startup.cs
@@ -32,7 +32,11 @@ namespace DockerExample.WebApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello in world of cointainers");
+                });
+                endpoints.MapGet("/2", async context =>
+                {
+                    await context.Response.WriteAsync("2.0 version");
                 });
             });
         }
